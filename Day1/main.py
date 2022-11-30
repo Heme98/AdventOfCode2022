367b3bcb9ff1920502aff1185b40da38ce7dc88f
Heme98 <herman.bergstrom98@gmail.com>
2022-11-30 15:04:20 +0100

    Added First file

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+# Helper functions:
+
+# Generic printer function to print all types
+def debug(value):
+    print(str(value))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    debug([1,2,3,4,])
