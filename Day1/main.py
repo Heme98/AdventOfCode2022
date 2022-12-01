ad4c1f56386e5fac7d03dad3936c52cdb78297fb
Heme98 <herman.bergstrom98@gmail.com>
2022-12-01 11:08:23 +0100

    Completed Day 1

--- Day1/main.py
+++ Day1/main.py
@@ -1,10 +1,32 @@
-# Helper functions:
+# AoC 2022 - Heme98
+import Helpers.parser
 
-# Generic printer function to print all types
-def debug(value):
-    print(str(value))
+
+# return a sorted list with all elves total calories
+def sumAllGroups(inputList):
+    totalCalories = []
+    currentCalories = 0
+    for calories in inputList:
+        if calories == "":
+            totalCalories.append(currentCalories)
+            currentCalories = 0
+        else:
+            currentCalories += int(calories)
+    return sorted(totalCalories)
+
+
+# Part 1: get the elf with highest number of calories (last in list or use max())
+def maxCalories(inputList):
+    return sumAllGroups(inputList)[-1]
+
+
+# Part 2: get top 3 elfs with highest number of calories (last 3 in list)
+def top3MaxCalories(inputList, target):
+    return sum(sumAllGroups(inputList)[-target:])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    debug([1,2,3,4,])
+    inputList = Helpers.parser.fileToList("input")
+    Helpers.parser.debug(maxCalories(inputList))
+    Helpers.parser.debug(top3MaxCalories(inputList, 3))
---
+++ Helpers/parser.py
@@ -0,0 +1,13 @@
+# Generic printer function to convert all types to string and print them
+def debug(value):
+    print(str(value))
+
+
+# Read file line by line and store it in a list
+def fileToList(name):
+    return [line.strip() for line in open(f"{name}.txt", "r")]
+
+
+# Read file line by line and store it in a set
+def fileToSet(name):
+    return set(line.split() for line in open(f"{name}.txt", "r"))
