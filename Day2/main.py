ebc67643c34c4b7493cc6df6087236df88a77936
Heme98 <Herminatorb@gmail.com>
2022-12-02 09:55:47 +0100

    Cleanup Day 2

--- Day2/main.py
+++ Day2/main.py
@@ -4,29 +4,28 @@ import Helpers.utils
 class Rules:
     WIN = 6
     DRAW = 3
-    map = {"A": 1, "B": 2, "C": 3,
-           "X": 1, "Y": 2, "Z": 3}
-
     choices = ["X", "Y", "Z"]
 
 def calculateWinner(challenge, user):
-    if Rules.map[challenge] == Rules.map[user]:
+    if (Rules.choices.index(challenge) + 1) == (Rules.choices.index(user) + 1):
         return Rules.DRAW
-    elif (Rules.map[challenge] + 1) % 3 == Rules.map[user] % 3:
+    elif ((Rules.choices.index(challenge) + 1) + 1) % 3 == (Rules.choices.index(user) + 1) % 3:
         return Rules.WIN
     return 0
 
 def PartOne(inputList):
     finalScore = 0
     for pair in inputList:
-        finalScore += Rules.map[pair[1]] + calculateWinner(pair[0], pair[1])
+        convertLetter = chr(ord(pair[0]) + 23) # Convert A, B, C to X, Y, Z
+        finalScore += (Rules.choices.index(pair[1]) + 1) + calculateWinner(convertLetter, pair[1])
     return finalScore
 
 def PartTwo(inputList):
     finalScore = 0
     for pair in inputList:
-        choice = Rules.choices[(Rules.map[pair[0]] + Rules.map[pair[1]]) % 3]
-        finalScore += Rules.map[choice] + calculateWinner(pair[0], choice)
+        convertLetter = chr(ord(pair[0]) + 23) # Convert A, B, C to X, Y, Z
+        choice = Rules.choices[((Rules.choices.index(convertLetter) + 1) + (Rules.choices.index(pair[1]) + 1)) % 3]
+        finalScore += (Rules.choices.index(choice) + 1) + calculateWinner(convertLetter, choice)
     return finalScore
 
 if __name__ == '__main__':
