04a8a8e6915ffc4ae9b81abad35c252d3830e6bb
Heme98 <herman.bergstrom98@gmail.com>
2022-12-07 20:58:34 +0100

    Cleanup Day 7

--- Day7/main.py
+++ Day7/main.py
@@ -17,23 +17,21 @@ def build(input):
             curr = curr.parent
         elif data[0] == "$" and data[1] == "cd" and data[2] != "/":  # enter directory
             curr = [d for d in curr.children if d.name == data[2]][0]
-        elif data[0] == "dir" or data[0].isdigit():  # if file or directory
+        elif data[0] == "dir" or data[0].isdigit():  # if file or directory (the add to current directory)
             size = int(data[0]) if data[0].isdigit() else 0
-            curr.children.append(Directory(curr, data[1], size))  # add file or directory
+            curr.children.append(Directory(curr, data[1], size))  # In this case, file is just a directory with a size
     return root
 
 def calculateStorage(root, storage):
-    total = calculateSubStorage(root, storage)
-    return total, storage
+    return calculateSubStorage(root, storage), storage  # return total storage and list of storage for subdirectories
 
-def calculateSubStorage(root, scores): # recursively go through each directory (when leaf reached return size)
-    val = sum(calculateSubStorage(child, scores) if child.children else child.storage for child in root.children)
-    scores.append(val)
+def calculateSubStorage(root, sizes):  # recursively go through each directory (when leaf reached return size)
+    val = sum(calculateSubStorage(child, sizes) if child.children else child.storage for child in root.children)
+    sizes.append(val)  # finished calulating directory size, append to sizes
     return val
 
 def partOne(file):
-    total, storage = calculateStorage(build(file), [])
-    return sum(x for x in storage if x < 100000)
+    return sum(x for x in calculateStorage(build(file),[])[1] if x < 100000)
 
 def partTwo(file):
     total, storage = calculateStorage(build(file), [])
