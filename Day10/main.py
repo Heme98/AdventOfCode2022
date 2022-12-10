152de3bc15c3f9282d4157b0d4d423803d899ad7
Heme98 <herman.bergstrom98@gmail.com>
2022-12-10 10:17:48 +0100

    Cleanup Day 10

--- Day10/main.py
+++ Day10/main.py
@@ -1,29 +1,30 @@
 # AoC 2022 - Heme98
 import Helpers.utils
 
-def noop(currentCRT, regx, clock_cycles, cycles, CRT, resetCycles, signals):
-    currentCRT += "#" if clock_cycles in [regx - 1, regx, regx + 1] else "."
+def noop(currentCRT, regx, clock_cycles, cycles, CRT, isCRT, signals):
+    if isCRT:
+        currentCRT += "#" if clock_cycles in [regx - 1, regx, regx + 1] else "."
     clock_cycles += 1
     if clock_cycles in cycles:
         CRT.append(currentCRT)
-        if resetCycles:
+        if isCRT:
             clock_cycles = 0
         currentCRT = ""
         signals.append(clock_cycles * regx)
     return currentCRT,regx, clock_cycles
 
-def calculateSignal(file,cycles, resetCycles=False):
+def calculateSignal(file,cycles, isCRT=False):
     signals,CRT = [], []
     clock_cycles, regx = 0, 1
     currentCRT = ""
     for item in file:
         if item[0] == "addx":
             for i in range(2):
-                currentCRT, regx, clock_cycles = noop(currentCRT, regx, clock_cycles, cycles, CRT, resetCycles, signals)
+                currentCRT, regx, clock_cycles = noop(currentCRT, regx, clock_cycles, cycles, CRT, isCRT, signals)
                 if i == 1:
                     regx += int(item[1])
         else:
-            currentCRT, regx, clock_cycles = noop(currentCRT,regx,clock_cycles, cycles, CRT, resetCycles, signals)
+            currentCRT, regx, clock_cycles = noop(currentCRT,regx,clock_cycles, cycles, CRT, isCRT, signals)
     return sum(signals), CRT
 
 def partOne(file):
@@ -36,5 +37,5 @@ def partTwo(file):
 
 if __name__ == '__main__':
     file = Helpers.utils.fileToListSplit("input.txt", " ")
-    Helpers.utils.debug2("Day 9, Part 1: ", partOne(file))
-    Helpers.utils.debug2("Day 9, Part 2: ", partTwo(file))
+    Helpers.utils.debug2("Day 10, Part 1: ", partOne(file))
+    Helpers.utils.debug2("Day 10, Part 2: ", partTwo(file))
