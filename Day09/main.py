413461c4ce82a0bb496031cc88e8ea171c0c2b79
Heme98 <herman.bergstrom98@gmail.com>
2022-12-10 11:47:02 +0100

    Cleanup day 9

--- Day09/main.py
+++ Day09/main.py
@@ -3,42 +3,28 @@ import Helpers.utils
 
 visited = []
 
-def istouchinghorizont(tail, head):
-    return abs(tail[0] - head[0]) in [0, 1]
-
-def istouchingvert(tail, head):
-    return abs(tail[1] - head[1]) in [0, 1]
+def istouching(tail, head):
+    return (abs(tail[0] - head[0]) in [0, 1]) and (abs(tail[1] - head[1]) in [0, 1])
 
 def move(rope, direction):
-    if direction == "R":
-        rope[0] += 1
-    if direction == "L":
-        rope[0] -= 1
-    if direction == "U":
-        rope[1] += 1
-    if direction == "D":
-        rope[1] -= 1
+    rope[0] += 1 if direction == "R" else 0
+    rope[0] -= 1 if direction == "L" else 0
+    rope[1] += 1 if direction == "U" else 0
+    rope[1] -= 1 if direction == "D" else 0
 
 def step(ropes, direction, amount):
     for _ in range(amount):
         for j in range(len(ropes)):
             if j == 0:
                 move(ropes[j], direction) # Update Head
-            elif not istouchinghorizont(ropes[j], ropes[j-1]):
+            elif not istouching(ropes[j], ropes[j-1]): # if not touching, update following knots
                 if ropes[j][1] != ropes[j-1][1]:
                     vertical = "U" if ropes[j][1] < ropes[j - 1][1] else "D"
                     move(ropes[j], vertical)
                 if ropes[j][0] != ropes[j - 1][0]:
                     horizontal = "R" if ropes[j][0] < ropes[j - 1][0] else "L"
                     move(ropes[j], horizontal)
-            elif not istouchingvert(ropes[j], ropes[j-1]):
-                if ropes[j][1] != ropes[j - 1][1]:
-                    vertical = "U" if ropes[j][1] < ropes[j - 1][1] else "D"
-                    move(ropes[j], vertical)
-                if ropes[j][0] != ropes[j-1][0]:
-                    horizontal = "R" if ropes[j][0] < ropes[j - 1][0] else "L"
-                    move(ropes[j], horizontal)
-            if [ropes[-1][0],ropes[-1][1]] not in visited:
+            if [ropes[-1][0],ropes[-1][1]] not in visited: # last knot (tail) to visited
                 visited.append([ropes[-1][0], ropes[-1][1]])
 
 def partOne(file):
