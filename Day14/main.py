3dde41be854cf96c4abf83759e18e07c7879f841
Heme98 <herman.bergstrom98@gmail.com>
2022-12-14 22:52:26 +0100

    Cleanup Day 14

--- Day14/main.py
+++ Day14/main.py
@@ -6,64 +6,48 @@ def buildCave(file, max_x, max_y):
     for item in file:
         previousPair = None
         for pair in item:
-            if previousPair is None:
-                previousPair = pair
-                continue
-            else:
+            if previousPair is not None:
                 if int(previousPair[0]) < int(pair[0]):
                     for i in range(int(previousPair[0]), int(pair[0])+1):
                         cave[int(previousPair[1])][i] = "#"
-                        previousPair = pair
                 elif int(previousPair[0]) > int(pair[0]):
                     for i in range(int(pair[0]), int(previousPair[0])+1):
                         cave[int(previousPair[1])][i] = "#"
-                        previousPair = pair
                 if int(previousPair[1]) < int(pair[1]):
                     for i in range(int(previousPair[1]), int(pair[1])+1):
                         cave[i][int(previousPair[0])] = "#"
-                        previousPair = pair
                 elif int(previousPair[1]) > int(pair[1]):
                     for i in range(int(pair[1]), int(previousPair[1])+1):
                         cave[i][int(previousPair[0])] = "#"
-                        previousPair = pair
+            previousPair = pair
     return cave
 
-def fillSand(cave, part1):
+def fillSand(cave, part1, count = 0):
     sandCoord = [0, 500]
     current = sandCoord
     condition = current[0] < len(cave)-1 if part1 else cave[current[0]][current[1]] != "o"
     while condition:
         if cave[current[0] + 1][current[1]] == ".":
             current = [current[0] + 1, current[1]]
+        elif cave[current[0] + 1][current[1] - 1] == ".":
+            current = [current[0] + 1, current[1] - 1]
+        elif cave[current[0] + 1][current[1] + 1] == ".":
+            current = [current[0] + 1, current[1] + 1]
         else:
-            while True:
-                if cave[current[0] + 1][current[1] - 1] == ".":
-                    current = [current[0] + 1, current[1] - 1]
-                    break
-                elif cave[current[0] + 1][current[1] + 1] == ".":
-                    current = [current[0] + 1, current[1] + 1]
-                    break
-                else:
-                    cave[current[0]][current[1]] = "o"
-                    current = sandCoord
-                    break
+            cave[current[0]][current[1]] = "o"
+            count += 1
+            current = sandCoord
         condition = current[0] < len(cave) - 1 if part1 else cave[current[0]][current[1]] != "o"
-    count = 0
-    for row in cave:
-        # print(row)
-        for item in row:
-            if item == "o":
-                count += 1
     return count
 
 def partOne(file, max_x, max_y):
-    cave = buildCave(file,max_x*2, max_y)
+    cave = buildCave(file,max_x, max_y)
     return fillSand(cave, True)
 
 def partTwo(file, max_x, max_y):
     cave = buildCave(file, max_x*2, max_y)
-    cave.append(["."] * (max_x*2+1)) # Create space
-    cave.append(["#"] * (max_x*2 + 1))  # Create bottom
+    cave.append(["."] * (max_x*2+1))  # Create space (multiple width of 2 to simulate infinity)
+    cave.append(["#"] * (max_x*2 + 1))  # Create bottom (multiple width of 2 to simulate infinity)
     return fillSand(cave, False)
 
 if __name__ == '__main__':
